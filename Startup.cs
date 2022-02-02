2b2a2a63924afa8471c269c5b1dd407832f068fd
nikol_vyskvarko <nikavuskvarko@gmail.com>
2022-02-02 12:57:42 +0300

    Deleting mocks

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shop.Data.Interfaces;
-using Shop.Data.Mocks;
 using Shop.Data.Models;
 using Shop.Data.Repository;
 
